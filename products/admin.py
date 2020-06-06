a9aa5c353ddaa5332ad03b6ed9248230be0678dd
paulonteri <onteripaul@gmail.com>
2020-06-06 17:25:18 +0300

    Added the product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Item, Category, SubCategory
 
-# Register your models here.
+admin.site.register(Item)
+admin.site.register(Category)
+admin.site.register(SubCategory)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    #
+    time_added = models.DateTimeField(
+        auto_now_add=True)
+    time_last_edited = models.DateTimeField(
+        auto_now=True)
+    #
+    last_edited_by = models.ForeignKey('accounts.User', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
+
+
+class SubCategory(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    #
+    time_added = models.DateTimeField(
+        auto_now_add=True)
+    time_last_edited = models.DateTimeField(
+        auto_now=True)
+    #
+    last_edited_by = models.ForeignKey('accounts.User', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.title} of {self.category.title}'
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    description = models.TextField()
+    image = models.ImageField()
+    #
+    time_added = models.DateTimeField(
+        auto_now_add=True)
+    time_last_edited = models.DateTimeField(
+        auto_now=True)
+    #
+    last_edited_by = models.ForeignKey('accounts.User', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
