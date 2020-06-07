1e20ec11c3bca775389cb8fdf74830f42d26874f
paulonteri <onteripaul@gmail.com>
2020-06-07 11:46:40 +0300

    added Payments and Coupon model

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Payment, Coupon
 
-# Register your models here.
+admin.site.register(Payment)
+admin.site.register(Coupon)
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from products.models import CommonModelInfo
 
-# Create your models here.
+
+class Payment(models.Model):
+    stripe_charge_id = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, blank=True, null=True)
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Coupon(CommonModelInfo):
+    code = models.CharField(max_length=15)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.code
+
+
+
