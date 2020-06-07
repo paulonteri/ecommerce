17fa938b47c6a2288a57a433cc6c520f6250389f
paulonteri <onteripaul@gmail.com>
2020-06-07 20:00:02 +0300

    added orders serializers

---
+++ orders/serializers/clients.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+
+from orders.models import OrderItem, Order
+from payments.serializers.clients import CouponSerializer
+from products.serializers.clients import ItemSerializer, ItemVariationDetailSerializer
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    item_variations = serializers.SerializerMethodField()
+    item = serializers.SerializerMethodField()
+    final_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = OrderItem
+        fields = (
+            'id',
+            'item',
+            'item_variations',
+            'quantity',
+            'final_price'
+        )
+
+    def get_item(self, obj):
+        return ItemSerializer(obj.item).data
+
+    def get_item_variations(self, obj):
+        return ItemVariationDetailSerializer(obj.item_variations.all(), many=True).data
+
+    def get_final_price(self, obj):
+        return obj.get_final_price()
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = serializers.SerializerMethodField()
+    total = serializers.SerializerMethodField()
+    coupon = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Order
+        fields = (
+            'id',
+            'order_items',
+            'total',
+            'coupon'
+        )
+
+    def get_order_items(self, obj):
+        return OrderItemSerializer(obj.items.all(), many=True).data
+
+    def get_total(self, obj):
+        return obj.get_total()
+
+    def get_coupon(self, obj):
+        if obj.coupon is not None:
+            return CouponSerializer(obj.coupon).data
+        return None
