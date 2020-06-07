e5a8542b8c1aaa90f108aa56fad128200d03951b
paulonteri <onteripaul@gmail.com>
2020-06-07 10:54:27 +0300

    Added the product brand model

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Item, Category, SubCategory
+from .models import Item, Category, SubCategory, Brand
 
 admin.site.register(Item)
 admin.site.register(Category)
 admin.site.register(SubCategory)
+admin.site.register(Brand)
--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=50, unique=True)
+    title = models.CharField(max_length=30, unique=True)
     #
     time_added = models.DateTimeField(
         auto_now_add=True)
@@ -30,11 +30,26 @@ class SubCategory(models.Model):
         return f'{self.title} of {self.category.title}'
 
 
+class Brand(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    #
+    time_added = models.DateTimeField(
+        auto_now_add=True)
+    time_last_edited = models.DateTimeField(
+        auto_now=True)
+    #
+    last_edited_by = models.ForeignKey('accounts.User', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
+
+
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
     discount_price = models.FloatField(blank=True, null=True)
-    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.PROTECT)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
     description = models.TextField()
     image = models.ImageField()
     #
