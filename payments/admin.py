9e422f5500732377ad305a56346c7bdbb3ff5b0c
paulonteri <onteripaul@gmail.com>
2020-06-07 12:06:00 +0300

    Added Refund model

--- payments/admin.py
+++ payments/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Payment, Coupon
+from .models import Payment, Coupon, Refund
 
 admin.site.register(Payment)
 admin.site.register(Coupon)
+admin.site.register(Refund)
--- payments/models.py
+++ payments/models.py
@@ -20,4 +20,11 @@ class Coupon(CommonModelInfo):
         return self.code
 
 
+class Refund(models.Model):
+    order = models.ForeignKey('orders.Order', on_delete=models.CASCADE)
+    reason = models.TextField()
+    accepted = models.BooleanField(default=False)
+    email = models.EmailField()
 
+    def __str__(self):
+        return f"{self.pk}"
