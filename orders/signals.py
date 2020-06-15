f18a661e5f7b0cb22a39b9d9435c695a4117d381
paulonteri <onteripaul@gmail.com>
2020-06-15 14:29:11 +0300

    signal to mark OrderItems as ordered if Order is ordered

--- orders/signals.py
+++ orders/signals.py
@@ -1,10 +1,14 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-from orders.models import Order
-
-
-def add_order_items(instance, **kwargs):
-    print('yow')
+from orders.models import Order, OrderItem
 
 
-m2m_changed.connect(add_order_items, sender=Order.items.through)
+@receiver(post_save, sender=Order)
+def after_save_order(instance, **kwargs):
+    # mark OrderItems as ordered if Order is ordered
+    if instance.ordered:
+        order_items = instance.items.all()
+        for item in order_items:
+            item.ordered = True
+        OrderItem.objects.bulk_update(order_items, ['ordered'])
