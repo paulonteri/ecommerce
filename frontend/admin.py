74ce836982bc768fd80d2cd05362f9c81f27bb71
Paul Onteri <onteripaul@gmail.com>
2020-07-24 22:59:45 +0300

    Add frontend app

---
+++ frontend/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+
+# This app will be mostly used for producing the frontend (HTML)
---
+++ frontend/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+# This app will be mostly used for producing the frontend (HTML)
