c6f06711f12d30ebd2832d053e6c24146d70a052
Paul Onteri <onteripaul@gmail.com>
2020-07-24 23:39:08 +0300

    Home View

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('accounts.urls')),
     path('api/v1/payments/', include('payments.urls')),
---
+++ frontend/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from frontend.views.home import index
+
+urlpatterns = [
+    path('', index, name='home/index'),
+]
