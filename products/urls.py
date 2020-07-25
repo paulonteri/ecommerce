e9518998b6feb8433114217b3ff90d554e8985fb
Paul Onteri <onteripaul@gmail.com>
2020-07-25 15:39:30 +0300

    add category and catalog

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from products.views import index
+from products.views import index, catalog, categories
 
 urlpatterns = [
     path('product', index, name='products/product'),
+    path('catalog', catalog, name='products/catalog'),
+    path('categories', categories, name='products/categories'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'products/index.html', {})
+
+
+def catalog(request):
+    return render(request, 'catalog/index.html', {})
+
+
+def categories(request):
+    return render(request, 'categories/index.html', {})
