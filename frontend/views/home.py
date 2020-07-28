2d7a3dbe72163cfa58e300467690d2f8284088d1
Paul Onteri <onteripaul@gmail.com>
2020-07-28 15:13:16 +0300

    reformat code

--- frontend/views/home.py
+++ frontend/views/home.py
@@ -4,9 +4,6 @@ from products.selectors.items import get_homepage_items
 
 
 def index(request):
-
     display_items = get_homepage_items()
-
     context = {'display_items': display_items}
-
     return render(request, 'home/index.html', context=context)
