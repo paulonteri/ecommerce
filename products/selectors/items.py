463f1049c62d04540b4ace7751152adcaa2d2950
Paul Onteri <onteripaul@gmail.com>
2020-07-28 17:42:38 +0300

    product detail

--- products/selectors/items.py
+++ products/selectors/items.py
@@ -1,4 +1,7 @@
+from django.db.models import Q
 from django.db.models.query import QuerySet
+
+from django.core.validators import validate_slug
 
 from products.models import Item
 
@@ -36,17 +39,39 @@ def get_item_detail(item_slug: str) -> Item:
     :param item_slug: products.models.Item.slug
     :returns: products.models.Item
     """
+    validate_slug(item_slug)
+
     item = Item.objects.get(slug__exact=item_slug)
 
     return item
+
+
+def get_item_related_products(item_slug: str) -> QuerySet:
+    """
+    Gets a particular Item info from it's slug
+    :param item_slug: products.models.Item.slug
+    :returns: Queryset with item's related_products
+    """
+
+    validate_slug(item_slug)
+
+    # TODO: Implement Logic
+    items = Item.objects.order_by("-time_last_edited") \
+                .filter(~Q(slug=item_slug))[:15] \
+        .only(
+        "title", "price", "discount_price", "slug", "description",
+        "image", "brand__title", "sub_category__title")
+    return items
 
 
 def get_item_full_detail(item_slug: str) -> dict:
     """
     Gets a particular Item info from it's slug
     :param item_slug: products.models.Item.slug
-    :returns: an Item with all related info including scpecifications, reviews, accessories
+    :returns: an Item with all related info including scpecifications, reviews, related_products
     """
+    validate_slug(item_slug)
+
     item = get_item_detail(item_slug)
     item.sub_category_title = item.sub_category.title.capitalize()
     item.brand_title = item.brand.title.capitalize()
@@ -54,14 +79,14 @@ def get_item_full_detail(item_slug: str) -> dict:
     item.sub_category_category_title = item.sub_category.category.title.capitalize()
 
     specifications = {}
-    accessories = []
+    related_products = get_item_related_products(item_slug)
     reviews = []
     q_a = []
 
     item_info = {
         "item": item,
         "specifications": specifications,
-        "accessories": accessories,
+        "related_products": related_products,
         "reviews": reviews,
         "q_a": q_a
     }
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
+from django.http import Http404
 from django.shortcuts import render
 
+from products.models import Item
 from products.selectors.items import get_item_full_detail
 
 
@@ -7,17 +9,22 @@ def product_detail(request, item_slug):
     """
     Renders the Product Detail Page
     """
-    item_info = get_item_full_detail(item_slug)
 
-    context = {
-        'item': item_info["item"],
-        "speciications": item_info["specifications"],
-        "accessories": item_info["accessories"],
-        "reviews": item_info["reviews"],
-        "q_a": item_info["q_a"]
-    }
+    try:
+        item_info = get_item_full_detail(item_slug)
 
-    print(context)
+        context = {
+            'item': item_info["item"],
+            "specifications": item_info["specifications"],
+            "related_products": item_info["related_products"],
+            "reviews": item_info["reviews"],
+            "q_a": item_info["q_a"]
+        }
+
+        print(context)
+    except Item.DoesNotExist:
+        raise Http404("Items does not exist")
+
     return render(request, 'products/product_detail.html', context=context)
 
 
@@ -35,10 +42,3 @@ def categories(request):
 
 def subcategory(request):
     return render(request, 'categories/subcategory.html', {})
-
-# def detail(request, poll_id):
-#     try:
-#         p = Poll.objects.get(pk=poll_id)
-#     except Poll.DoesNotExist:
-#         raise Http404("Poll does not exist")
-#     return render(request, 'polls/detail.html', {'poll': p})
