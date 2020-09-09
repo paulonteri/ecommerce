396cd37f17ba01dea33fa7eccc5c5829cea23dba
Paul Onteri <onteripaul@gmail.com>
2020-09-09 12:09:25 +0300

    Add User form to Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,33 @@
 from django.contrib import admin
-from .models import Address
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User, Address
+
+
+class UserAdmin(UserAdmin):
+    # Display
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'is_admin',)}),
+    )
+
+    # Create User
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'is_admin',)}),
+    )
+
+    # List display
+    list_display = ('username', 'email', 'is_active', 'is_admin', 'is_staff',)
+    list_filter = ('is_admin', 'is_active', 'is_superuser', 'is_staff',)
+
+    search_fields = ('username', 'email',)
+    ordering = ('username', 'email',)
+
+
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Address)
